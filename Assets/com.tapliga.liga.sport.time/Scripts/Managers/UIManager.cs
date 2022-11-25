70dc9bbcd9744a6579b99276c8dbb0ad27850238
Sladkiy Eduard <nikcxprojects@gmail.com>
2022-11-25 14:36:21 +0300

    update

--- Assets/com.tapliga.liga.sport.time/Scripts/Managers/UIManager.cs
+++ Assets/com.tapliga.liga.sport.time/Scripts/Managers/UIManager.cs
@@ -1,12 +1,36 @@
-using UnityEngine.UI;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    private GameObject _last = null;
+
+    [SerializeField] GameObject menu;
+    [SerializeField] GameObject options;
+    [SerializeField] GameObject top;
     [SerializeField] GameObject game;
+    [SerializeField] GameObject result;
 
     private void Awake()
     {
-        
+        OpenWindow(0);
+    }
+
+    public void OpenWindow(int windowIndex)
+    {
+        if(_last)
+        {
+            _last.SetActive(false);
+        }
+
+        switch(windowIndex)
+        {
+            case 0: _last = menu; break;
+            case 1: _last = options; break;
+            case 2: _last = top; break;
+            case 3: _last = game; break;
+            case 4: _last = result; break;
+        }
+
+        _last.SetActive(true);
     }
 }
